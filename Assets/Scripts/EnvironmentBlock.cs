ff4bea23f06b984aaab53ae9b2586db39d0cd25e
Samuele Saviano <s.saviano@simultria.nl>
2024-12-05 16:10:59 +0100

    base environment movement

---
+++ Assets/Scripts/EnvironmentBlock.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Wave.Environment
+{
+    public class EnvironmentBlock : MonoBehaviour
+    {
+        [SerializeField] private Transform _leftSocket;
+        [SerializeField] private Transform _rightSocket;
+
+        public float Width => Vector3.Distance(_leftSocket.position, _rightSocket.position);
+        public Vector3 Position
+        {
+            get => transform.position;
+            set => transform.position = value;
+        }
+
+        public void Place(int index)
+        {
+            Position = new Vector3(0, 0, index * Width);
+            SetActive(true);
+        }
+
+        public void Move(float speed)
+        {
+            Position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        public void Recycle(EnvironmentBlock lastBlock)
+        {
+            Position = new Vector3(0, 0, lastBlock.Position.z + Width);
+            SetActive(true);
+        }
+
+        public void SetActive(bool active) => gameObject.SetActive(active);
+    } 
+}
---
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Wave.Environment;
+
+namespace Wave.Managers
+{
+    public class EnvironmentManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _initialBlockPrefab;
+        [SerializeField] private GameObject[] _prefabs;
+        [SerializeField] private int _poolCapacity = 10;
+        [SerializeField] private int _maxBlocks = 3;
+        [SerializeField] private float _speed = 5f;
+
+        private List<EnvironmentBlock> _blocks = new List<EnvironmentBlock>();
+        private Queue<EnvironmentBlock> _pool = new Queue<EnvironmentBlock>();
+
+        private void Awake()
+        {
+            InitializePool();
+        }
+
+        private void Start()
+        {
+            SpawnInitialBlocks();
+        }
+
+        private void Update()
+        {
+            MoveBlocks();
+            RecycleBlocks();
+        }
+
+        private void InitializePool()
+        {
+            for (int i = 0; i < _poolCapacity; i++)
+            {
+                AddBlock(_prefabs[Random.Range(0, _prefabs.Length)]);
+            }
+        }
+
+        private void AddBlock(GameObject prefab)
+        {
+            GameObject clone = Instantiate(prefab, transform);
+            clone.SetActive(false);
+            _pool.Enqueue(clone.GetComponent<EnvironmentBlock>());
+        }
+
+        private void SpawnInitialBlocks()
+        {
+            EnvironmentBlock block = GetInitialBlock();
+            block.Place(0);
+            _blocks.Add(block);
+
+            for (int i = 0; i < _maxBlocks; i++)
+            {
+                block = GetBlockFromPool();
+                block.Place(i + 1);
+
+                _blocks.Add(block);
+            }
+        }
+
+        private void MoveBlocks()
+        {
+            foreach (EnvironmentBlock block in _blocks)
+                block.Move(_speed);
+        }
+
+        private void RecycleBlocks()
+        {
+            if (_blocks.Count == 0)
+                return;
+
+            EnvironmentBlock firstBlock = _blocks[0];
+            EnvironmentBlock newBlock;
+
+            if (firstBlock.Position.z < -firstBlock.Width)
+            {
+                firstBlock.SetActive(false);
+                _pool.Enqueue(firstBlock);
+                _blocks.RemoveAt(0);
+
+                newBlock = GetBlockFromPool();
+                newBlock.Recycle(_blocks[^1]);
+                _blocks.Add(newBlock);
+            }
+        }
+
+        private EnvironmentBlock GetInitialBlock()
+        {
+            if (_initialBlockPrefab == null)
+                return GetBlockFromPool();
+
+            GameObject blockObject = Instantiate(_initialBlockPrefab, transform);
+
+            if (blockObject.TryGetComponent(out EnvironmentBlock block))
+                return block;
+
+            return null;
+        }
+
+        private EnvironmentBlock GetBlockFromPool()
+        {
+            if (_pool.Count > 0)
+                return _pool.Dequeue();
+
+            GameObject prefab = _prefabs[Random.Range(0, _prefabs.Length)];
+            GameObject blockObject = Instantiate(prefab, transform);
+
+            if (blockObject.TryGetComponent(out EnvironmentBlock block))
+                return block;
+
+            return null;
+        }
+    } 
+}
+
